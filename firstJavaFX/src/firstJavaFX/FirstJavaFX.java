05f5adf020d85f073a14d193af441e83fa59fb10
pawej <biuro@websystemy.pl>
2014-10-06 22:05:52 +0200

    First commit

---
+++ firstJavaFX/src/firstJavaFX/FirstJavaFX.java
@@ -0,0 +1,18 @@
+package firstJavaFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FirstJavaFX extends Application {
+
+	public static void main(String[] args) {
+		launch(args);		
+	}
+	
+
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		stage.show();
+	}
+}
